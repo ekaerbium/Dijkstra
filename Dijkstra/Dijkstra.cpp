5550355737c7802f6fb95c4c85eb7250baee90e8
ekaerbium <bobafett2000@gmail.com>
2016-09-04 19:20:59 -0600

    Optimized

--- Dijkstra/Dijkstra.cpp
+++ Dijkstra/Dijkstra.cpp
@@ -11,20 +11,19 @@
 #include <fstream>
 #include <thread>
 #include <atomic>
-#define V 200
+#define V 100
 #define BRF 50
 
-
-double eucDist(std::tuple<int, int, int> p1, std::tuple<int, int, int> p2)
+using namespace std;
+double eucDist(tuple<int, int, int> &p1, tuple<int, int, int> &p2)
 {
-	using namespace std;
 	return sqrt(pow(get<0>(p1) - get<0>(p2), 2.0)
 		+ pow(get<1>(p1) - get<1>(p2), 2.0)
 		+ pow(get<2>(p1) - get<2>(p2), 2.0));
 
 }
 
-int minDistance(std::vector<double> dist, std::vector<bool> sptSet)
+int minDistance(vector<double> &dist, vector<bool> &sptSet)
 {
 
 	int min = INT_MAX, min_index;
@@ -39,82 +38,13 @@ int minDistance(std::vector<double> dist, std::vector<bool> sptSet)
 	return min_index;
 }
 
-void printSolution(std::vector<int> dist, std::vector<int> prev, int fin)
-{
-	int finish = fin;
-	for (int i = 0; i < V; i++)
-		std::cout << "Node:" << i << " Dist:" << dist[i] << " Prev:" << prev[i] << "\n";
-
-	bool atStrt = false;
-	std::vector<int> path;
-	while (!atStrt)
-	{
-		path.insert(path.begin(), prev[fin]);
-		fin = prev[fin];
-		if (fin == 0 || fin == INT_MAX)
-		{
-			atStrt = true;
-		}
-	}
-	if(fin != INT_MAX)
-	{
-		for (int i = 0; i < path.size(); i++)
-		{
-			std::cout << path[i] << "->";
-		}
-		std::cout << finish << "\n";
-	}
-	else
-	{
-		std::cout << "No paths to node " << finish << "\n";
-	}
-}
-
-/*void dijkstra(std::vector<std::vector<int>> graph, int src)
-{
-	std::vector<int> dist (V, INT_MAX);     // The output array.  dist[i] will hold the shortest distance from src to i
-	
-	std::vector<bool> sptSet (V, false); // sptSet[i] will true if vertex i is included in shortest path tree or shortest distance from src to i is finalized
-	
-	std::vector<int> prev (V, INT_MAX); //Holds previous node on shortest path. Used to reconstruct path from start to finish
-
-	// Distance of source vertex from itself is always 0
-	dist[src] = 0;
-
-	// Find shortest path for all vertices
-	for (int count = 0; count < V - 1; count++)
-	{
-		// Pick the minimum distance vertex from the set of vertices not
-		// yet processed. u is always equal to src in first iteration.
-		int u = minDistance(dist, sptSet);
-
-		// Mark the picked vertex as processed
-		sptSet[u] = true;
-
-		// Update dist value of the adjacent vertices of the picked vertex.
-		for (int v = 0; v < V; v++)
-
-			// Update dist[v] only if is not in sptSet, there is an edge from 
-			// u to v, and total weight of path from src to  v through u is 
-			// smaller than current value of dist[v]
-			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
-			{
-				dist[v] = dist[u] + graph[u][v];
-				prev[v] = u;
-
-			}
-
-	}
-
-	
-}*/
 //Dijkstra based pathfinding to single node
-std::vector<int> astar(std::vector<std::vector<double>> graph, std::vector<std::tuple<int,int,int>> map, int src, int fin)
+vector<int> astar(vector<vector<double>> &graph, vector<tuple<int,int,int>> &map, int src, int fin)
 {
-	std::vector<double> dist(graph.size(), INT_MAX);
-	std::vector<double> heur(graph.size(), INT_MAX);
-	std::vector<bool> sptSet(graph.size(), false);
-	std::vector<int> prev(graph.size(), INT_MAX); 
+	vector<double> dist(graph.size(), INT_MAX);
+	vector<double> heur(graph.size(), INT_MAX);
+	vector<bool> sptSet(graph.size(), false);
+	vector<int> prev(graph.size(), INT_MAX); 
 	dist[src] = 0;
 	heur[src] = eucDist(map[src], map[fin]);
 	bool finished = false;
@@ -137,28 +67,28 @@ std::vector<int> astar(std::vector<std::vector<double>> graph, std::vector<std::
 
 	}
 	bool atStrt = false;
-	std::vector<int> path;
+	vector<int> path;
 	path.push_back(fin);
 	while (!atStrt)
 	{
 		path.insert(path.begin(), prev[fin]);
 		fin = prev[fin];
-		if (fin == src || fin == INT_MAX)
+		if (fin == src)
 		{
 			atStrt = true;
 		}
+		if (fin == INT_MAX)
+		{
+			path.clear();
+			path.push_back(INT_MAX);
+			atStrt = true;
+		}
 	}
-	if (fin == INT_MAX)
-	{
-		path.clear();
-		path.push_back(INT_MAX);
-	}
-		return path;
+	return path;
 }
 
-std::vector<std::vector<double>> graphGen(std::vector<std::tuple<int,int,int>> nodeMap, int brF)
+vector<vector<double>> graphGen(vector<tuple<int,int,int>> nodeMap, int brF)
 {
-	using namespace std;
 	vector<vector<double>> nodes(V, vector<double>(V, 0));
 	random_device rand;
 	for (int i = 0; i < V; ++i)
@@ -174,20 +104,11 @@ std::vector<std::vector<double>> graphGen(std::vector<std::tuple<int,int,int>> n
 			}
 		}
 	}
-	/*for (int i = 0; i < V; ++i)
-	{
-		for (int j = 0; j < V; ++j)
-		{
-			cout << nodes[i][j] << " ";
-		}
-		cout << "\n";
-	}*/
 	return nodes;
 }
 
-std::vector<std::tuple<int, int, int>> mapGen()
+vector<tuple<int, int, int>> mapGen()
 {
-	using namespace std;
 	random_device rd;
 	vector<tuple<int, int, int>> nodeMap;
 	for (int i = 0; i < V; ++i)
@@ -197,10 +118,10 @@ std::vector<std::tuple<int, int, int>> mapGen()
 	return nodeMap;
 }
 
-void thAStar(std::vector<std::vector<double>> nodes, std::vector<std::tuple<int, int, int>> map, std::vector<std::vector<std::vector<int>>>::iterator row, std::vector<std::vector<std::vector<int>>>::iterator rowFin, std::vector<std::vector<std::vector<int>>>::iterator setBegin)
+void thAStar(vector<vector<double>> &nodes, vector<tuple<int, int, int>> &map, vector<vector<vector<int>>>::iterator &row, vector<vector<vector<int>>>::iterator &rowFin, vector<vector<vector<int>>>::iterator &setBegin)
 {
-	std::cout << "Init thread\n";
-	std::vector<std::vector<int>>::iterator col;
+	cout << "Init thread\n";
+	vector<vector<int>>::iterator col;
 	for (row; row != rowFin; ++row)
 	{
 		for (col = row->begin(); col != row->end(); ++col)
@@ -212,7 +133,6 @@ void thAStar(std::vector<std::vector<double>> nodes, std::vector<std::tuple<int,
 
 int main()
 {
-	using namespace std;
 	time_t startCalc = time(NULL);
 	vector<tuple<int, int, int>> nodeMap = mapGen();
 	vector<vector<double>> nodes = graphGen(nodeMap, BRF);
@@ -270,3 +190,76 @@ int main()
 	cout << "Written to file in " << endWrite-endCalc << " seconds\n";
 	return 0;
 }
+
+
+
+
+/*void printSolution(std::vector<int> dist, std::vector<int> prev, int fin)
+{
+int finish = fin;
+for (int i = 0; i < V; i++)
+std::cout << "Node:" << i << " Dist:" << dist[i] << " Prev:" << prev[i] << "\n";
+
+bool atStrt = false;
+std::vector<int> path;
+while (!atStrt)
+{
+path.insert(path.begin(), prev[fin]);
+fin = prev[fin];
+if (fin == 0 || fin == INT_MAX)
+{
+atStrt = true;
+}
+}
+if(fin != INT_MAX)
+{
+for (int i = 0; i < path.size(); i++)
+{
+std::cout << path[i] << "->";
+}
+std::cout << finish << "\n";
+}
+else
+{
+std::cout << "No paths to node " << finish << "\n";
+}
+}
+
+void dijkstra(std::vector<std::vector<int>> graph, int src)
+{
+std::vector<int> dist (V, INT_MAX);     // The output array.  dist[i] will hold the shortest distance from src to i
+
+std::vector<bool> sptSet (V, false); // sptSet[i] will true if vertex i is included in shortest path tree or shortest distance from src to i is finalized
+
+std::vector<int> prev (V, INT_MAX); //Holds previous node on shortest path. Used to reconstruct path from start to finish
+
+// Distance of source vertex from itself is always 0
+dist[src] = 0;
+
+// Find shortest path for all vertices
+for (int count = 0; count < V - 1; count++)
+{
+// Pick the minimum distance vertex from the set of vertices not
+// yet processed. u is always equal to src in first iteration.
+int u = minDistance(dist, sptSet);
+
+// Mark the picked vertex as processed
+sptSet[u] = true;
+
+// Update dist value of the adjacent vertices of the picked vertex.
+for (int v = 0; v < V; v++)
+
+// Update dist[v] only if is not in sptSet, there is an edge from
+// u to v, and total weight of path from src to  v through u is
+// smaller than current value of dist[v]
+if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
+{
+dist[v] = dist[u] + graph[u][v];
+prev[v] = u;
+
+}
+
+}
+
+
+}*/
